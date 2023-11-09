ecfe3ac65a29f307955206443209ccb315a2c0ed
Alexander <aleacosta@hacienda.gov.do>
2023-11-09 10:22:43 -0400

    Hello World

---
+++ src/main/java/helloworld/Greeter.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package helloworld;
+
+/**
+ * @author jhosep
+ *
+ */
+public class Greeter {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	String getGreeting() {
+        return "Hello, World!";
+    }
+
+}
