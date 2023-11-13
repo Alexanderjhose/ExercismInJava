1d3a89f27bcd60f3e325ecec379bf5ef07620fbb
Alexander <aleacosta@hacienda.gov.do>
2023-11-13 08:51:14 -0400

    complete Log Levels

--- src/main/java/loglevels/LogLevels.java
+++ src/main/java/loglevels/LogLevels.java
@@ -15,19 +15,8 @@ public class LogLevels {
 
 	public static void main(String[] args) {
 
-//		System.out.println(message("[ERROR]: Invalid operation"));
-//
-//		System.out.println(message("[WARNING]: Disk almost full\r\n"));
-
-//		System.out.println(logLevel("[ERROR]: Invalid operation"));
-
-		System.out.println(reformat("[INFO]: Operation completed"));
-
-//		LogLevels.reformat("[INFO]: Operation completed")
-		// => "Operation completed (info)"
-
-//		System.out.println("Operation completed (info)");
-
+		//tip your code here...
+		
 	}
 
 	public static String message(String logLine) {
@@ -41,8 +30,7 @@ public class LogLevels {
 	}
 
 	public static String reformat(String logLine) {
-		return message(logLine);
-
+		return message(logLine) + " (" + logLevel(logLine) + ")";
 	}
 
 }
